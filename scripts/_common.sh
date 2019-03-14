0330150c88c691944ac78702076bd1749bbb808b
yalh76 <yalh@yahoo.com>
2019-03-14 01:15:26 +0100

    Apply last example_ynh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 #=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+pkg_dependencies="gettext postgresql postgresql-contrib libpq-dev git curl gcc make openssl libssl-dev pkg-config"
+
+#=================================================
 # PERSONAL HELPERS
 #=================================================
 
