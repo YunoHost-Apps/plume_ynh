c6db1e0f8feb58205138418ccbf4ee7183180c71
yalh76 <yalh@yahoo.com>
2019-03-21 05:53:01 +0100

    fix secret key creation

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="gettext postgresql postgresql-contrib libpq-dev git curl gcc make openssl libssl-dev pkg-config"
+pkg_dependencies="gettext postgresql postgresql-contrib libpq-dev git curl gcc make openssl libssl-dev pkg-config openssl"
 
 #=================================================
 # PERSONAL HELPERS
